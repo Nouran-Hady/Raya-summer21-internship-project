4e08543ee1cf02651b29549fee26f723930a6c1c
Nouran Hady <nouranhady21@gmail.com>
2021-08-24 12:00:52 +0200

    integration

---
+++ initConfig.py
@@ -0,0 +1,177 @@
+import serial,time,xlrd,openpyxl,os
+
+#########################
+# Nihal and Hadeer#
+#########################
+
+def open_connection(port):
+    console = serial.Serial(port=port)
+    if console.isOpen():
+        print("serial connection sucessfully")
+        console.write(b"en \n")
+        time.sleep(1)
+        console.write(b"terminal length 0\n")
+        time.sleep(1)
+        console.write(b"show ip int br\n")
+        time.sleep(2)
+        return console
+    else:
+        print("Sorry! you cannot connect")
+        return 0
+
+def read_from_console(console):
+    numBytes = console.inWaiting()
+    print("num",numBytes)
+    if numBytes:
+        data = console.read(numBytes)
+        return data.decode()
+    else:
+        return False
+
+def run_command(console, command='\n',sleep=3):
+    print("Sending Command: " + command)
+    console.write(command.encode())
+    time.sleep(sleep)
+
+def check_init_dialog(console):
+    run_command(console)
+    output = read_from_console(console)
+    print("out",output)
+    if 'initial configuration dialog?' in output:
+        run_command(console,'no')
+        run_command(console,'\n',15)
+        run_command(console, '\r\n')
+        return True
+    else:
+        return False
+
+#########################
+# Eman#
+#########################
+# import xlrd
+def configure(name,user_name,user_pass,ip_address,index):
+    list = ['COM2', 'COM1']
+    with serial.Serial(port=list[index]) as console:
+        if console.isOpen():
+            print("Serial connection successfully")
+            console.write(b'enable\n')
+            time.sleep(2)
+            console.write(b'configure terminal\n')
+            time.sleep(2)
+            console.write(b'enable secret cisco\n')
+            time.sleep(2)
+            console.write(b'hostname '+name.encode()+b'\n')
+            time.sleep(2)
+            console.write(b'ip domain-name local\n')
+            time.sleep(2)
+            console.write(b'crypto key generate rsa\n')
+            time.sleep(2)
+            console.write(b'2048\n')
+            time.sleep(2)
+            console.write(b'ip ssh version 2\n')
+            time.sleep(2)
+            console.write(b'username '+user_name.encode()+b' password '+user_pass.encode()+b'\n')
+            time.sleep(2)
+            console.write(b'line vty 0 4\n')
+            time.sleep(2)
+            console.write(b'transport input telnet ssh\n')
+            time.sleep(2)
+            console.write(b'login local\n')
+            time.sleep(2)
+            console.write(b'exit\n')
+            time.sleep(2)
+            console.write(b'interface VLAN 1\n')
+            time.sleep(2)
+            console.write(b'ip address ' +ip_address.encode()+ b' 255.255.255.0\n')
+            time.sleep(2)
+            console.write(b'no shutdown\n')
+            time.sleep(2)
+            console.write(b'exit\n')
+            time.sleep(10)
+        else:
+            print('Sorry')
+
+#########################
+# Nouran#
+#########################
+
+filelist=[]
+interfaces=[]
+porttype=[]
+vlans=[]
+
+def switch_vlan(port,vlan_sheet):
+    file=open('switch port type.txt','a+')
+    workbook = openpyxl.load_workbook(filename=vlan_sheet)
+    sheet= workbook['Sheet1']
+    for row in sheet.iter_rows(min_row=2,min_col=2):
+        for i in row:
+            file.write(str(i.value)+'\n')
+    file.close()
+
+    try:
+            file = open('switch port type.txt', 'r')
+            for i in file.readlines():
+                filelist.append(i.strip('\n'))
+
+            for i in range(3):
+                for j in range(4):
+                    interfaces.append('interface ethernet ' + f'{i}/{j}')
+
+            for i in range(len(filelist)):
+                if i % 2 == 0:
+                    porttype.append(filelist[i])
+                else:
+                    vlans.append(filelist[i])
+            interfaces.pop(11)
+            interfaces.pop(10)
+            interfaces.pop(9)
+            file.close()
+
+            with serial.Serial(port=port) as switch:
+                i = 0
+                for index in porttype:
+
+                    if index.lower() == 'access':
+                        vlan =f'vlan {vlans[i]}'
+
+                        switch.write(vlan.encode()+b'\n')
+                        time.sleep(3)
+                        switch.write(b'exit\n')
+                        time.sleep(2)
+                        ethernet = interfaces[i]
+
+                        switch.write(ethernet.encode()+b'\n')
+                        time.sleep(2)
+
+                        switch.write(b'switchport mode access\n')
+                        time.sleep(2)
+                        switch.write(b'no shut\n')
+                        time.sleep(2)
+                        vlan=f'switchport access vlan {vlans[i]}'
+                        switch.write(vlan.encode()+b'\n')
+                        time.sleep(2)
+                        switch.write(b'exit\n')
+                        i = i + 1
+
+                    else:
+                        ethernet = interfaces[i]
+
+                        switch.write(ethernet.encode()+b'\n')
+                        time.sleep(2)
+                        switch.write(b'no shut\n')
+                        time.sleep(2)
+                        switch.write(b'switchport trunk encapsulation dot1q\n')
+                        time.sleep(2)
+                        switch.write(b'switchport mode trunk\n')
+                        time.sleep(2)
+                        vlan=f'switchport trunk allowed Vlan {vlans[i]}'
+                        switch.write(vlan.encode()+b'\n')
+                        time.sleep(2)
+                        switch.write(b'exit\n')
+                        i = i + 1
+
+
+    except Exception as error:
+            print(error)
+    os.remove(r'D:\raya summer training\raya project\pythonProject\switch port type.txt')
---
+++ networkAutomation.py
@@ -0,0 +1,109 @@
+import xlrd
+import initConfig as init
+import serial
+
+# Nihal and Hadeer
+
+file = xlrd.open_workbook("Routers_data.xlsx")
+
+sheets = file.sheet_by_name("Sheet1")
+
+outputFile1 = open("router1ConfigText.txt", "w")
+outputFile2 = open("router2ConfigText.txt", "w")
+
+firstTime1 = 0
+firstTime2 = 0
+index = 1
+while index < sheets.nrows:
+    if sheets.row_values(index)[0] == 'R1':
+        outFile = outputFile1
+    else:
+        outFile = outputFile2
+    if not sheets.row_values(index)[0] == "":
+        if firstTime1 == 0 or firstTime2 == 0:
+            outFile.write(f'''
+en
+conf t
+hostname {sheets.row_values(index)[0]}
+enable secret {sheets.row_values(index)[3]}
+ip domain-name local
+crypto key generate rsa
+2048
+ip ssh version 2
+username {sheets.row_values(index)[1]} password {sheets.row_values(index)[2]}
+line vty 0 4
+transport input telnet ssh
+login local
+''')
+            if sheets.row_values(index)[0] == 'R1':
+                firstTime1 += 1
+            else:
+                firstTime2 += 1
+        outFile.write(f'''
+interface ethernet {int(sheets.row_values(index)[4])}/0
+ip address {sheets.row_values(index)[5].split('/')[0]} 255.255.255.0
+no shutdown
+''')
+    index += 1
+
+outputFile1.write(f'''
+exit
+''')
+
+outputFile2.write(f'''
+exit
+''')
+
+outputFile1.close()
+outputFile2.close()
+
+# con = init.open_connection('COM4')
+# if con:
+#     init.check_init_dialog(con)
+#     init.run_command(con)
+#     with open('router1ConfigText.txt','r+') as file:
+#         for cmd in file:
+#             init.run_command(con, cmd)
+#
+# output = init.read_from_console(con)
+# print(output)
+#
+#
+# con = init.open_connection('COM5')
+# if con:
+#     init.check_init_dialog(con)
+#     init.run_command(con)
+#     with open('router2ConfigText.txt','r+') as file:
+#         for cmd in file:
+#             init.run_command(con, cmd)
+#
+# output = init.read_from_console(con)
+# print(output)
+#
+# # Eman
+#
+# workbook = xlrd.open_workbook('Switches_SSH-data.xlsx')
+# worksheet = workbook.sheet_by_name('Sheet1')
+# ind=0
+# i=1
+#
+#
+# while i < worksheet.nrows:
+#     data = worksheet.row_values(i)
+#     back_slash = data[5].find('/')
+#     ip_address = data[5][0:back_slash]
+#     init.configure(data[0],data[1],data[2],ip_address,ind)
+#     ind+=1
+#     i+= 1
+#
+# #Nouran
+# init.switch_vlan('COM2','switch 1 Ports.xlsx')
+# init.switch_vlan('COM1','Switch 2 Ports .xlsx')
+#
+# # dist subnet source
+# with serial.Serial(port='COM4') as rout:
+#     rout.write(b'ip route 192.168.10.5 255.255.255.0 10.1.1.1 ')
+# with serial.Serial(port='COM5') as rout:
+#     rout.write(b'ip route 10.1.1.5 255.255.255.0 192.168.10.1')
+#
+#
